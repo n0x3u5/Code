b19c338bf70fcfb0c338632590446981d894207c
Nakshatra Mukhopadhyay <nakshatra.mukhopadhyay1994@gmail.com>
2015-08-15 17:06:03 +0530

    Finished UniqueChars

--- Java Programs/Home/ArrayString/UniqueChars.java
+++ Java Programs/Home/ArrayString/UniqueChars.java
@@ -1,41 +1,144 @@
-package com.n0x3u5.ArrayString;
-
 import java.io.*;
-import java.util.*;
+import java.util.Scanner;
+import java.math.BigInteger;
 
-class UniqueCharChecker {
-    private String inputString;
+class SimpleUniqueCharChecker {
 
-    UniqueCharChecker() {
+    protected String inputString;
+    protected int strLen;
+
+    SimpleUniqueCharChecker() {
         inputString = "";
     }
 
-    private boolean hasUniqueChars() {
+    public boolean hasUniqueChars() {
+        //Assuming ASCII characters. Not Unicode.
+        boolean[] charExists = new boolean[256];
+        for (int i = 0;i < inputString.length();i ++ ) {
+            int currentAscii = inputString.charAt(i);
+            if(charExists[currentAscii]) {
+                return false;
+            }
+            charExists[currentAscii] = true;
+        }
         return true;
     }
 
     public void setInputString(String input) {
         inputString = input;
+        strLen = inputString.length();
     }
 
     public String getInputString() {
         return inputString;
     }
+
+}
+
+class BruteUniqueCharChecker extends SimpleUniqueCharChecker {
+    public boolean hasUniqueChars() {
+        for (int i=0;i < strLen;i++) {
+            for (int j=i+1;j < strLen;j++) {
+                if(inputString.charAt(i)==inputString.charAt(j)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
+
+class SortedUniqueCharChecker extends SimpleUniqueCharChecker {
+
+    void insertIntArray(int[] intArray, char[] charArray) {
+        for(int i = 0;i<strLen;i ++) {
+            intArray[i] = (int)charArray[i];
+        }
+    }
+
+    void insertCharArray(int[] intArray, char[] charArray) {
+        for(int i = 0;i<strLen;i ++) {
+            charArray[i] = (char)intArray[i];
+        }
+    }
+
+    void sortChars(int[] intArray) {
+
+        for(int i = 1;i<strLen;i ++) {
+            int j = i;
+            while (j > 0 && intArray[j-1] > intArray[j]) {
+                intArray[j] = intArray[j] + intArray[j-1];
+                intArray[j-1] = intArray[j] - intArray[j-1];
+                intArray[j] = intArray[j] - intArray[j-1];
+                j --;
+            }
+        }
+
+    }
+
+    public boolean hasUniqueChars() {
+
+        int[] intArray = new int[strLen];
+        char[] charArray = inputString.toCharArray();
+
+        insertIntArray(intArray, charArray);
+
+        sortChars(intArray);
+
+        insertIntArray(intArray, charArray);
+
+        for(int i = 0;i<strLen-1;i ++) {
+            if(charArray[i] == charArray[i+1]) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+
 }
 
 class StringDriver {
 
-    public static void main(String []args) {
+    public static void main(String[] args) {
         String inputString;
 
         Scanner input = new Scanner(System.in);
-        UniqueCharChecker checkUnique = new UniqueCharChecker();
+        SimpleUniqueCharChecker checkSimpleUnique = new SimpleUniqueCharChecker();
+        SortedUniqueCharChecker checkSortedUnique = new SortedUniqueCharChecker();
+        BruteUniqueCharChecker checkBruteUnique = new BruteUniqueCharChecker();
 
-        System.out.println("Enter a string: ");
+        System.out.print("\nEnter a string: ");
         inputString = input.nextLine();
 
-        checkUnique.setInputString(inputString);
+        checkSimpleUnique.setInputString(inputString);
+        checkSortedUnique.setInputString(inputString);
+        checkBruteUnique.setInputString(inputString);
 
-        System.out.println("The input was: " + checkUnique.getInputString());
+        System.out.println("\n---------------------------------------");
+        System.out.println("Via SimpleUniqueChars:");
+        if(checkSimpleUnique.hasUniqueChars()) {
+            System.out.println("The string has all unique characters.");
+        } else {
+            System.out.println("The string has non - unique characters.");
+        }
+        System.out.println("---------------------------------------");
+
+        System.out.println("Via SortedUniqueChars:");
+        if(checkSortedUnique.hasUniqueChars()) {
+            System.out.println("The string has all unique characters.");
+        } else {
+            System.out.println("The string has non - unique characters.");
+        }
+        System.out.println("---------------------------------------");
+
+        System.out.println("Via BruteUniqueChars:");
+        if(checkBruteUnique.hasUniqueChars()) {
+            System.out.println("The string has all unique characters.");
+        } else {
+            System.out.println("The string has non - unique characters.");
+        }
+        System.out.println("---------------------------------------");
     }
+
 }
