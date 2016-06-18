b968f840f698ba61c79d97559f5b3e854d9d0add
Nakshatra Mukhopadhyay <nakshatra.mukhopadhyay1994@gmail.com>
2016-06-19 01:46:39 +0530

    Listed posts using ng-repeat

--- Node/FlapperNews/app.js
+++ Node/FlapperNews/app.js
@@ -5,8 +5,15 @@
 var FlapperNews;
 (function (FlapperNews) {
     'use strict';
-    var app = angular.module('flapperNews', []).controller('MainCtrl', function NewsController($scope) {
-        $scope.test = "Hello Uranus!";
+    var app = angular.module('flapperNews', []);
+    app.controller("MainCtrl", function MainCtrl($scope) {
+        $scope.posts = [
+            { title: 'post 1', upvotes: 5 },
+            { title: 'post 2', upvotes: 2 },
+            { title: 'post 3', upvotes: 15 },
+            { title: 'post 4', upvotes: 9 },
+            { title: 'post 5', upvotes: 4 }
+        ];
     });
 })(FlapperNews || (FlapperNews = {}));
 //# sourceMappingURL=app.js.map
--- Node/FlapperNews/app.ts
+++ Node/FlapperNews/app.ts
@@ -5,7 +5,14 @@
  */
 module FlapperNews {
     'use strict';
-    var app = angular.module('flapperNews', []).controller('MainCtrl', function NewsController($scope) {
-        $scope.test = "Hello Uranus!";
+    let app : ng.IModule = angular.module('flapperNews', []);
+    app.controller("MainCtrl", function MainCtrl($scope) {
+        $scope.posts = [
+            {title: 'post 1', upvotes: 5},
+            {title: 'post 2', upvotes: 2},
+            {title: 'post 3', upvotes: 15},
+            {title: 'post 4', upvotes: 9},
+            {title: 'post 5', upvotes: 4}
+        ]
     });
 }
